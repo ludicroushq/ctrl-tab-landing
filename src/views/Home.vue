09c78cc22e29acc2c2fbe28f2686932585b864e7
Manthan Mallikarjun <nahtnam@gmail.com>
2018-09-17 23:40:24 -0700

    add buttons to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,19 @@
               <h2 class="subtitle">
                 ...is an extension that shows the data that matters to you in your new tab page.
               </h2>
+              <div class="columns">
+                <div class="column">
+                  <a href="https://chrome.google.com/webstore/detail/tab/ijfnjhlohbehhlhhokgokabonpnlldon" class="button is-medium is-fullwidth is-info" target="_blank" rel="noopener noreferrer">View on Chrome Store</a>
+                </div>
+                <div class="column">
+                  <a
+                    class="button is-medium is-fullwidth is-success"
+                    onclick="chrome.webstore.install()"
+                  >
+                    Install Extension
+                  g</a>
+                </div>
+              </div>
             </div>
             <div class="column">
               <img src="https://raw.githubusercontent.com/ludicrousxyz/tab/master/public/img/screenshot.png">
